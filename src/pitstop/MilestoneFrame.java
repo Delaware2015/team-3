447edda2963db096a5903862f76c2dbc50e3e204
Joseph Fortebuono <joeyfort@udel.edu>
2015-10-31 04:12:48 -0400

    progress on milestones

--- src/pitstop/MilestoneFrame.java
+++ src/pitstop/MilestoneFrame.java
@@ -11,17 +11,34 @@ public class MilestoneFrame extends Screen {
     final static boolean shouldWeightX = true;
     final static boolean RIGHT_TO_LEFT = false;
     
-    final static int FONT_SIZE1 = 40;
-    final static int FONT_SIZE2 = 20;
+    final static int FONT_SIZE1 = 60;
+    final static int FONT_SIZE2 = 15;
+    final static int FONT_SIZE3 = 35;
+
 
 
     public MilestoneFrame(){
       	super();
+      	JPanel background = new JPanel();
+      	background.setLayout(new BoxLayout(background,BoxLayout.Y_AXIS));
+      	
         JPanel panel = new JPanel();
-        panel.setPreferredSize(new Dimension(Main.WIDTH, 500));
+       // panel.setPreferredSize(new Dimension(Main.WIDTH, 500));
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));  
         addComponentsToPane(panel);
-        this.getContentPane().add(panel);
+        
+        JPanel navi1 = new JPanel();
+        JPanel navi2 = new JPanel();
+        navi1.add(new JButton("Profile"));
+        navi2.add(new JButton("Calander"));
+        navi2.add(new JButton("Milestones"));
+        navi2.add(new JButton("Colleges"));
+     
+        
+        background.add(panel);
+        background.add(navi1);
+       background.add(navi2);
+        this.getContentPane().add(background);
     }
     public static void addComponentsToPane(Container pane) {
         if (RIGHT_TO_LEFT) {
@@ -42,14 +59,13 @@ public class MilestoneFrame extends Screen {
         c.gridy = 0;
         pane.add(label, c);
         
-        checkbox = new JCheckBox("Pre-Standardized Test (SAT)");
-        checkbox.setFont(new Font(label.getFont().getName(), Font.PLAIN, FONT_SIZE2));        
+        label = new JLabel("High School");
+        label.setFont(new Font(label.getFont().getName(), Font.PLAIN, FONT_SIZE3));        
         c.gridx = 0;
         c.gridy = 1;
-        removeListeners(checkbox);
-        pane.add(checkbox, c);
+        pane.add(label, c);
         
-        checkbox = new JCheckBox("Standardized Test (SAT)");
+        checkbox = new JCheckBox("Freshman");
         checkbox.setSelected(true);
         checkbox.setFont(new Font(label.getFont().getName(), Font.PLAIN, FONT_SIZE2));        
         c.gridx = 0;
@@ -57,27 +73,85 @@ public class MilestoneFrame extends Screen {
         removeListeners(checkbox);
         pane.add(checkbox, c);
         
-        checkbox = new JCheckBox("American College Testing (ACT)");
+        checkbox = new JCheckBox("Sophmore");
+        checkbox.setSelected(true);
         checkbox.setFont(new Font(label.getFont().getName(), Font.PLAIN, FONT_SIZE2));        
         c.gridx = 0;
         c.gridy = 3;
         removeListeners(checkbox);
         pane.add(checkbox, c);
         
-        checkbox = new JCheckBox("Teacher Recommendations");
+        checkbox = new JCheckBox("Junior");
         checkbox.setFont(new Font(label.getFont().getName(), Font.PLAIN, FONT_SIZE2));        
         c.gridx = 0;
         c.gridy = 4;
         removeListeners(checkbox);
         pane.add(checkbox, c);
         
-        checkbox = new JCheckBox("Your mom");
-        checkbox.setSelected(true);
+        checkbox = new JCheckBox("Senior");
         checkbox.setFont(new Font(label.getFont().getName(), Font.PLAIN, FONT_SIZE2));        
         c.gridx = 0;
         c.gridy = 5;
         removeListeners(checkbox);
         pane.add(checkbox, c);
+        
+        label = new JLabel("Testing");
+        label.setFont(new Font(label.getFont().getName(), Font.PLAIN, FONT_SIZE3));        
+        c.gridx = 0;
+        c.gridy = 6;
+        pane.add(label, c);
+        
+        checkbox = new JCheckBox("Pre-Standardized Test (SAT)");
+        checkbox.setSelected(true);
+        checkbox.setFont(new Font(label.getFont().getName(), Font.PLAIN, FONT_SIZE2));        
+        c.gridx = 0;
+        c.gridy = 7;
+        removeListeners(checkbox);
+        pane.add(checkbox, c);
+        
+        checkbox = new JCheckBox("Standardized Test (SAT)");
+        checkbox.setSelected(true);
+        checkbox.setFont(new Font(label.getFont().getName(), Font.PLAIN, FONT_SIZE2));        
+        c.gridx = 0;
+        c.gridy = 8;
+        removeListeners(checkbox);
+        pane.add(checkbox, c);
+        
+        checkbox = new JCheckBox("American College Testing (ACT)");
+        checkbox.setSelected(true);
+        checkbox.setFont(new Font(label.getFont().getName(), Font.PLAIN, FONT_SIZE2));        
+        c.gridx = 0;
+        c.gridy = 9;
+        removeListeners(checkbox);
+        pane.add(checkbox, c);
+        
+        label = new JLabel("Applying");
+        label.setFont(new Font(label.getFont().getName(), Font.PLAIN, FONT_SIZE3));        
+        c.gridx = 0;
+        c.gridy = 10;
+        pane.add(label, c);
+        
+        checkbox = new JCheckBox("Teacher Recommendations");
+        checkbox.setFont(new Font(label.getFont().getName(), Font.PLAIN, FONT_SIZE2));        
+        c.gridx = 0;
+        c.gridy = 11;
+        removeListeners(checkbox);
+        pane.add(checkbox, c);
+        
+        checkbox = new JCheckBox("Common Application");
+        checkbox.setFont(new Font(label.getFont().getName(), Font.PLAIN, FONT_SIZE2));        
+        c.gridx = 0;
+        c.gridy = 12;
+        removeListeners(checkbox);
+        pane.add(checkbox, c);
+        
+        checkbox = new JCheckBox("College Letters");
+        checkbox.setFont(new Font(label.getFont().getName(), Font.PLAIN, FONT_SIZE2));        
+        c.gridx = 0;
+        c.gridy = 13;
+        removeListeners(checkbox);
+        pane.add(checkbox, c);
+        
     }
     
     private static void removeListeners(JCheckBox checkbox) {
