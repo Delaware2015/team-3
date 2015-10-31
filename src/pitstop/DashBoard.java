f627da1dd4d7c3481efa7ec42e5436fd613027de
motu454 <motu454@gmail.com>
2015-10-31 03:04:34 -0400

    More dashboard stuff

--- src/pitstop/DashBoard.java
+++ src/pitstop/DashBoard.java
@@ -1,7 +1,16 @@
 package pitstop;
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
 
 public class DashBoard extends Screen {
 	
@@ -20,7 +29,7 @@ public class DashBoard extends Screen {
 	
 	//initializes all methods
 	public void initialize(){
-		scrollFrame.setPreferredSize(new Dimension( 400,200));
+		scrollFrame.setPreferredSize(new Dimension(400,200));
 		MakeScreen();
 		
 	}
@@ -29,11 +38,20 @@ public class DashBoard extends Screen {
 	
 	//makes the screen with background panel, then other panels layered
 	public void MakeScreen(){
+	//jpanel 
 	JPanel Background = new JPanel();
 	Background.setBackground(Color.RED);
-	Background.setSize(400,600);
 	Background.setLayout(new BoxLayout(Background, BoxLayout.Y_AXIS));
 	Background.add(scrollFrame);
+	//container and placement
+	getContentPane().setLayout(new FlowLayout());
+	Container contentPane = getContentPane();
+    contentPane.add("TimeLine", Background);
+    //picture attempt
+    ImageIcon image = new ImageIcon("Pitstop/src/pitstop.images/Batman.jpg");
+    JLabel label = new JLabel("Profile", image, JLabel.CENTER);
+    JPanel panel = new JPanel(new BorderLayout());
+    panel.add( label, BorderLayout.CENTER );
 	} 
 
 }
